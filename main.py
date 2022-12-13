52f4d33feb89c23105fce2f6a4dcc2a8de1bc5c9
theSirD <108454328+theSirD@users.noreply.github.com>
2022-12-14 02:29:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 def calc(expression):
     allowed = '+-/*'
-    if sign in allowed:
-        try:
-            a, b = expression.split(sign)
-            a, b = int(a), int(b)
-            if sign == '+':
-                return a+b
-            if sign == '-':
-                return a-b
-            if sign == '/':
-                return a/b
-            if sign == '*':
-                return a*b
-        except(ValueError, TypeError):
-            raise ValueError("Некорректный ввод")
+    for sign in allowed:
+        if sign in allowed:
+            try:
+                a, b = expression.split(sign)
+                a, b = int(a), int(b)
+                if sign == '+':
+                    return a+b
+                if sign == '-':
+                    return a-b
+                if sign == '/':
+                    return a/b
+                if sign == '*':
+                    return a*b
+            except(ValueError, TypeError):
+                raise ValueError("Некорректный ввод")
 if __name__ == '__main__':
     calc('')
