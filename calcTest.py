3a7ff38b80d5b09eb44d5849d37ba0eefad473f6
theSirD <108454328+theSirD@users.noreply.github.com>
2022-12-14 02:47:38 +0300

    Update calcTest.py

--- calcTest.py
+++ calcTest.py
@@ -8,8 +8,8 @@ class calcTest(unittest.TestCase):
     def test_minus(self):
         self.assertEqual(calc('4 - 3'), 1)
 
-#     def test_mult(self):
-#         self.assertEqual(calc('3 * 4'), 12)
+    def test_mult(self):
+        self.assertEqual(calc('3 * 4'), 12)
 
-#     def test_div(self):
-#         self.assertEqual(calc('10 / 5'), 2)
+    def test_div(self):
+        self.assertEqual(calc('10 / 5'), 2)
