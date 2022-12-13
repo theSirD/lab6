d5544b865bdf4f2e39c19ac94d460a2dc28fae25
theSirD <108454328+theSirD@users.noreply.github.com>
2022-12-14 02:31:03 +0300

    Update calcTest.py

--- calcTest.py
+++ calcTest.py
@@ -3,13 +3,13 @@ from main import calc
 
 class calcTest(unittest.TestCase):
     def test_plus(self):
-        self.assertEqual(calc(3, 4, +), 7)
+        self.assertEqual(calc('3 + 4'), 7)
 
     def test_minus(self):
-        self.assertEqual(calc(4, 3, -), 1)
+        self.assertEqual(calc('4 - 3'), 1)
 
     def test_mult(self):
-        self.assertEqual(calc(3, 4, *), 12)
+        self.assertEqual(calc('3 * 4'), 12)
 
     def test_div(self):
-        self.assertEqual(calc(10, 5, /), 2)
+        self.assertEqual(calc('10 \ 5'), 2)
